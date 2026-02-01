919d4566e1324a86251d67321b184306186ee32e
ANBUSELVAM-S <anbuselvam.sk05@gmail.com>
2026-02-01 21:42:28 +0530

    Fix import case for LoginPage component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-import LoginPage from "./pages/LoginPage";
+import LoginPage from "./pages/Loginpage";
 import DashboardPage from "./pages/DashboardPage";
 import TaskPage from "./pages/TaskPage";
 import PendingPage from "./pages/PendingPage";
